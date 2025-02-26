ce275db70ecfbdc7b966873a04ce717f85864143
Akhilesh <akhilsharma3332@gmail.com>
2025-02-26 17:53:37 +0530

    [/referrals] query bug fixed .

--- router/refrral.py
+++ router/refrral.py
@@ -7,54 +7,13 @@ from ..models.referrals import Refrrals
 from ..utils.passwords import get_password_hash
 from ..utils.users import get_current_user
 from fastapi.responses import JSONResponse
+from sqlalchemy import cast, String
 
 router = APIRouter(
     prefix="",
     tags=["Refrral"],
     responses={404: {"description": "Not found"}},
-)  
-
-# class RefrralBase(BaseModel):
-#     email: str 
-
-# @router.post("/referr")
-# async def create_new_referral(refrral: RefrralBase, db: db_dependency, current_user: Users = Depends(get_current_user)): 
-
-#     refrral_body = Refrrals( 
-#             referrer_user_id=current_user.id,
-#             referred_user_email=refrral.email,  
-#         )
-
-#     try:
-
-#         db.add(refrral_body)
-#         db.commit()
-#         db.refresh(refrral_body) 
-        
-#         return JSONResponse(
-#             status_code=200,
-#             content={
-#                     "message": f"Successfully referred", 
-#                     "detail": f"you have refered the user with email: {refrral.email}, you can check the refrals"
-#                 }
-#         )
-
-#     except IntegrityError as e: 
-
-#         db.rollback()
-#         raise HTTPException(
-#             status_code=status.HTTP_400_BAD_REQUEST,
-#             detail= f"You have already referred {refrral.email}, there is no need of referr again." 
-#         )
-        
-#     except Exception as e:
-#         # Catch any other unexpected errors
-#         db.rollback()  
-#         raise HTTPException(
-#             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-#             detail=f"Failed referred. Exception: {str(e)}"
-#         )
-
+)
 
 
 @router.post("/referrals")
@@ -62,8 +21,9 @@ async def get_all_refrrals(db: db_dependency, current_user: Users = Depends(get_
 
     try:
 
-        results = db.query(Refrrals).filter(
-                        Refrrals.referrer_user_id == current_user.id).all();  
+        results = db.query(Refrrals).where(
+            cast(Refrrals.referrer_user_id, String) == str(current_user.id)
+        ).all()
         
         refered_users_list = []
         for referral in results: 
--- router/users.py
+++ router/users.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
-from typing import Annotated
-from pydantic import BaseModel
-from fastapi import APIRouter, Depends, HTTPException, status
+from typing import Annotated, Optional
+from pydantic import BaseModel, Field, EmailStr, constr
+from fastapi import APIRouter, Depends, HTTPException, status, Query
 from sqlalchemy.exc import IntegrityError
 from ..services.database import db_dependency
 from ..models.users import Users
@@ -9,7 +9,6 @@ from ..utils.passwords import get_password_hash
 from ..utils.users import get_current_user
 from fastapi.responses import JSONResponse
 from ..services.claim_refrral import claim_new_refrral_by
-from typing import Optional
 
 router = APIRouter(
     prefix="/users",
@@ -19,18 +18,15 @@ router = APIRouter(
 
 
 class UserBase(BaseModel): 
-    username: str
-    email: str
-    first_name: str
-    last_name: str
-    password: str
+    username: constr(min_length=3, max_length=50) = Field(..., description="Username must be between 3 and 50 characters.")
+    email: EmailStr = Field(..., description="Valid email address.")
+    first_name: constr(min_length=1, max_length=50) = Field(..., description="First name must be between 1 and 50 characters.")
+    last_name: constr(min_length=1, max_length=50) = Field(..., description="Last name must be between 1 and 50 characters.")
+    password: constr(min_length=8) = Field(..., description="Password must be at least 8 characters.")
 
 
 @router.post("/register")
-async def create_user( user: UserBase, db: db_dependency, referrer: Optional[str] = None):
-
-    
-    # TODO: Validate the users emails and username
+async def create_user(user: UserBase, db: db_dependency, referrer: Optional[str] = Query(None, description="Referrer username")):
 
     new_user = Users( 
             username=user.username,
@@ -48,7 +44,7 @@ async def create_user( user: UserBase, db: db_dependency, referrer: Optional[str
         db.refresh(new_user) 
         
         # Claiming the refral 
-        if(referrer):
+        if referrer:
             claim_new_refrral_by(referrer=referrer, 
                                  referred=new_user.id, 
                                  db=db)
@@ -67,7 +63,7 @@ async def create_user( user: UserBase, db: db_dependency, referrer: Optional[str
         db.rollback()
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
-            detail="A user with this email already exists."
+            detail="A user with this email or username already exists."
         )
         
     except Exception as e:
@@ -77,7 +73,6 @@ async def create_user( user: UserBase, db: db_dependency, referrer: Optional[str
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Failed to register user. Error: {str(e)}"
         )
-
 
 @router.post("/me", response_model=UserBase)
 async def read_users_me(db: db_dependency, current_user: Users = Depends(get_current_user)): 
